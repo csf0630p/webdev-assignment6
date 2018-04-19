3c4daa56b988e35a023717c60d08ba04f3e012d5
csf0630p <31044168+csf0630p@users.noreply.github.com>
2018-04-18 22:28:08 -0700

    step7

--- assignment/services/website.mock.server.js
+++
@@ -1,8 +0,0 @@
-var WEBSITES = [
-  {"_id": "456", "name": "Facebook", developerId: "123", "description": "Lorem"},
-  {"_id": "432", "name": "Twitter", developerId: "234", "description": "Lorem"},
-  {"_id": "234", "name": "Amazon", developerId: "345", "description": "Lorem"},
-  {"_id": "333", "name": "MyWebSite", developerId: "123", "description": "Lorem"}
-];
-
-module.exports = WEBSITES;
