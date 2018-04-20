a86f9aae7d9a62df5206bfdf7699eb20ee1a2a1d
csf0630p <31044168+csf0630p@users.noreply.github.com>
2018-04-19 20:42:54 -0700

    step8.3

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -40,10 +40,9 @@ export class ProfileComponent implements OnInit {
   }
 
   logout() {
-    return this.userService.logout().subscribe(
-      () => {
-        this.router.navigate(['/login']);
-      }
+    return this.userService.logout()
+      .subscribe(
+        (data: any) => this.router.navigate(['/login'])
     );
   }
 
